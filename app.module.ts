94dac819cd9516e868ee24a106e34cfb373e2b07
IamVSPA <sivaprasanna4ui@gmail.com>
2018-10-09 23:03:05 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core'; 
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { FormsModule } from '@angular/forms';
 import * as _ from 'lodash';
-import Swal from 'sweetalert2'
 import { ToastrModule } from 'ngx-toastr';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
 @NgModule({
@@ -15,6 +15,7 @@ import { ToastrModule } from 'ngx-toastr';
   imports: [
     BrowserModule,
     FormsModule,
+    AngularFontAwesomeModule,
     ToastrModule.forRoot(),
     ModalModule.forRoot()
   ],
