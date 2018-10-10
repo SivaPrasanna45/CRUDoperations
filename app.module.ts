bc985408e67424c42d6d752f9e68782259d6ca71
IamVSPA <sivaprasanna4ui@gmail.com>
2018-10-10 23:20:53 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule , Pipe, PipeTransform} from '@angular/core';
 import { AppComponent } from './app.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { FormsModule } from '@angular/forms';
 import * as _ from 'lodash';
 import { ToastrModule } from 'ngx-toastr';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,8 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
     BrowserModule,
     FormsModule,
     AngularFontAwesomeModule,
+    Ng2SearchPipeModule,
+    NgxPaginationModule,
     ToastrModule.forRoot(),
     ModalModule.forRoot()
   ],
